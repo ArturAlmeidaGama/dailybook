3bf9e40c79ec7deaaff345b353038403a99a7d0d
artur <arturgamagama044@gmail.com>
2024-05-15 21:25:49 -0300

    update 2

--- database/migrations/2024_04_25_000656_create_notes_table.php
+++ database/migrations/2024_04_25_000656_create_notes_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('annotation');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
